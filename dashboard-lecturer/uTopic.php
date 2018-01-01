02fcff7ee9b5bdb236aad873010f170d12b89d4e
krishnadhaneswara <34973556+krishnadhaneswara@users.noreply.github.com>
2018-01-01 14:19:46 +0800

    Add files via upload

---
+++ dashboard-lecturer/uTopic.php
@@ -0,0 +1,14 @@
+<?php
+
+include "../connection.php";
+
+$update=mysql_query("UPDATE topic SET topic = '$_POST[topic]' WHERE topicID='".$_POST[topicID]."'");
+if($update)
+{
+	header('location:topic.php');
+	}
+else
+{
+	header('location:vTopic.php');
+	}
+?>
